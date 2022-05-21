00a7853f282366d487c90cc6f6c5a32ba1d89622
pusheng <sdf753741789@gmail.com>
2022-05-22 05:51:07 +0800

    edit home page

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,6 +1,22 @@
+const Buy = require('../models/buy')
+const Item = require('../models/item')
+
 const homeController = {
-  gethome: (req, res) => {
-    res.render('home')
+  gethome: (req, res, next) => {
+    return Promise.all([Buy.find()
+      .populate(['itemId', 'userId'])
+      .populate({ path: 'itemId', populate: { path: 'categoryId' } })
+      .populate({ path: 'itemId', populate: { path: 'unitId' } }).lean(),
+    Item.find()
+      .populate(['categoryId', 'unitId']).lean()])
+      .then(([buys, items]) => {
+        const buyIsDone = buys.filter(obj => obj.isDone === false)
+        const itemObjs = items.filter(obj => obj.stock < obj.safeStock)
+        res.render('home', {
+          buyIsDone
+          , itemObjs
+        })
+      }).catch(err => next(err))
   }
 }
 
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,10 +1,15 @@
 const dayjs = require('dayjs') //載入 dayjs 套件
+
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime) //近期時間
+
 module.exports = {
   currentYear: () => dayjs().year(),
   currentYearMonDate: () => date = dayjs().format('YYYY/MM/DD--HH:mm'),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   },
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   percentAlert: function (a, b, options) {
     return a < b ? options.fn(this) : options.inverse(this)
   }
